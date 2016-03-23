6b6d7a5a2f5c0814e8626fa191806285dc99771f
Kit Kemper <cgkemper@gmail.com>
2016-03-23 17:28:23 -0400

    added tests and functionality for session management

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,18 +6,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		@user = users(:john)
 		@user = users(:jane)
 
-	test "login with invalid information" do
+	test "login with invalid information followed by logout" do
 		get login_path
 		post login_path, session: { email: @user.email, password: 'password' }
+		assert is_logged_in?
 		assert_redirected_to @user
 		follow_redirect!
 		assert_template 'users/show'
 		assert_select "a[href=?]", login_path, count: 0
 		assert_select "a[href=?]", logout_path
 		assert_select "a[href=?]", user_path(@user)
-		assert_not flash.empty?
-		get root_path
-		assert flash.empty?
-		
+		delete logout_path
+		assert_not is_logged_in?
+		assert_redirected_to root_url
+		# Simulate a user clicking logoyt in a second window.
+		delete logout_path
+		follow_redirect!
+		assert_select"a[href=?]", login_path
+		assert_select "a[href=?]", logout_path, count: 0
+		assert_select "a[href=?]", user_path(@user), count: 0		
 	end
 end
