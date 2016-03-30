823f7775f5ef1ac9ac66037ade511eddbd7d0b77
Kit Kemper <cgkemper@gmail.com>
2016-03-30 10:05:22 -0400

    Working on authorizations: make sure that the user is logged in before accessing anything, make sure that the correct user is logged in.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,16 @@ module SessionsHelper
 			session[:user_id] = user.id
 	end
 
-	#Remembers a user ina persitent session
+	#Remembers a user in a persitent session
 	def remember(user)
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	# Returns true if the given user is the current user.
+	def current_user?(user)
+		user == current_user
 	end
 
 
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,6 +9,15 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   	get edit_user_path(@user)
   	assert_template 'users/edit'
   	patch user_path(@user), user: { name: "", email: "foo@invalid", password: "foo", password_confirmation: "bar"}
-  	assert_template 'users/edit'  	
+  	assert_not flash.empty?
+  	assert_redirected to @user
+  	@user.reload
+  	assert_equal name, @user.name
+  	assert_equal email, @user.email 	
   end
+
+  test "successful edit" do
+  	log_in_as(@user)
+  	get edit_user_path(@user)
+  end	
 end
